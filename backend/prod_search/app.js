1e26ca061b23ce1831f691a5f00530987e90c6f2
rohit0407rai <rohit0407rai@gmail.com>
2023-03-16 15:00:23 +0530

    Forproduct

---
+++ backend/prod_search/app.js
@@ -0,0 +1,17 @@
+const axios = require("axios");
+
+const options = {
+  method: 'GET',
+  url: 'https://amazon23.p.rapidapi.com/product-search',
+  params: {query: 'xbox', country: 'US'},
+  headers: {
+    'X-RapidAPI-Key': 'c0e6b1bc88msh72b33926948d6fcp18712ejsn3dd37cfa9ae4',
+    'X-RapidAPI-Host': 'amazon23.p.rapidapi.com'
+  }
+};
+
+axios.request(options).then(function (response) {
+	console.log(response.data);
+}).catch(function (error) {
+	console.error(error);
+});
