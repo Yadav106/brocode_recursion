c293981d6fe072997efdb4f2c568fc3570df9533
BHAVYAPODDAR <poddarbhavya3@gmail.com>
2023-03-16 14:30:56 +0530

    bhavya end

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,39 +1,28 @@
 import React from 'react'
-<<<<<<< HEAD
+
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Landingpage from './components/Landingpage';
-import { Route } from 'react-router-dom';
-// import Login from '';
-=======
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Login from "./components/Login";
 import Signup from './components/Signup';
->>>>>>> 7ded1875e0010742f3350ef5fee8bba9ea5326d7
+
 
 const App = () => {
 
   return (
-<<<<<<< HEAD
-    <div>
-      <Header />
-      <Route path="/">
-        <Landingpage />
-      </Route>
-      <Route excat path="/Login">
-        <Login />
-      </Route>
-      <Footer />
+
+    <div>  
+      <BrowserRouter>
+
+        <Routes>
+          <Route path ="/" Component={Landingpage}/>
+          <Route path ="/Login" Component={Login}/>
+          <Route path='/signup' Component={Signup} />
+          <Route path="*" Component={Missing}/>
+        </Routes>
+      </BrowserRouter>
     </div>
-=======
-    <BrowserRouter>
-      <div>App</div>
-      <Login />
-      <Routes>
-        <Route path='/signup' Component={Signup} />
-      </Routes>
-    </BrowserRouter>
->>>>>>> 7ded1875e0010742f3350ef5fee8bba9ea5326d7
   )
 }
 
