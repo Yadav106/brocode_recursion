065447f4a8f74443eef8978985c39bae2c07c1e6
Yadav106 <sahil.jp.yadav@gmail.com>
2023-03-16 20:58:03 +0530

    search implemented

--- frontend/src/components/CatalogNav.jsx
+++ frontend/src/components/CatalogNav.jsx
@@ -3,27 +3,32 @@ import { BiSearchAlt } from 'react-icons/bi'
 import { HiMenuAlt3 } from 'react-icons/hi'
 import { AiOutlineShoppingCart } from 'react-icons/ai'
 
-const CatalogNav = () => {
+const CatalogNav = (props) => {
   return (
     <nav
         className={`
             flex
-            bg-[#ff6c02]
+            bg-[#23272A]
             h-[75px]
-            sticky
+            w-[100vw]
+            fixed
             p-[15px]
+            top-0
         `}
     >
         <div className='flex justify-between items-center w-[100vw]'>
-            <h1 className='text-4xl font-bold text-white'>{`Bro>>Shop`}</h1>
+            <h1 className='text-4xl font-bold text-white cursor-pointer' onClick={() => props.setSearched(false)}>{`Bro>>Shop`}</h1>
             <div className='flex justify-center items-center gap-[10px] bg-zinc-300 h-[60px] rounded-3xl p-[10px]'>
                 <input type='text' className={`
                     rounded-3xl
                     p-[10px]
                     text-[20px]
                     w-[650px]
-                `}/>
-                <BiSearchAlt className='text-4xl text-[#ff6c02] cursor-pointer hover:text-black'/>
+                `}
+                onChange={(e) => props.setSearchText(e.target.value)}/>
+                <BiSearchAlt className='text-4xl text-[#23272A] cursor-pointer hover:text-black' onClick={() => {
+                    props.getData(props.searchText)
+                }}/>
             </div>
             <div className='flex justify-center items-center gap-[20px]'>
                 <AiOutlineShoppingCart className='text-4xl text-white cursor-pointer hover:text-black'/>
